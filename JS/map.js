ac294ae8e74d3d0ff0e7d17c8b78526bd3060b5f
Alexandre BIDAUX <bidaal1910@iutparis.local>
2022-11-16 16:38:53 +0100

    Centre la carte sur la position actuelle

--- JS/map.js
+++ JS/map.js
@@ -4,21 +4,19 @@ let drawing = false;
 let dynamicPoly;
 
 function init() {
-    const map = L.map('map').setView([48.84158851946866, 2.2678810636885673], 12);
+    navigator.geolocation.getCurrentPosition(
+        (position) => setupMap([position.coords.latitude,position.coords.longitude]), 
+        (error) => setupMap([48.84158851946866, 2.2678810636885673]));
+}
+
+function setupMap(coord) {
+    console.log(coord)
+    const map = L.map('map').setView(coord, 15);
 
     const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-
-
-    const cercleConcorde= L.circle([48.865484005449844, 2.321141822687012],{
-                                    color: '#0000FF',
-                                    fillColor: 'A2A2FF',
-                                    opacity: 0.75,
-                                    radius : 70
-                                }).addTo(map).bindPopup("Place de la Concorde");
-                        
+    }).addTo(map);      
 
     $.getJSON("https://opendata.paris.fr/api/records/1.0/search/?dataset=fontaines-a-boire&q=&rows=10000&facet=type_objet&facet=modele&facet=commune&facet=dispo",
             (data) => {
